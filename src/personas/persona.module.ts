6b00bd2f226b41277228531f55eb5eddea8247c5
accel33 <accelwtf@gmail.com>
2024-04-05 09:32:32 -0500

    fix: bug en ruta de Dynamo

--- src/personas/persona.module.ts
+++ src/personas/persona.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PersonaController } from './persona.controller';
 import { PersonaService } from './persona.service';
-import { DynamoDBModule } from 'src/dynamo-db/dynamo-db.module';
+import { DynamoDBModule } from '../dynamo-db/dynamo-db.module';
 
 @Module({
   imports: [DynamoDBModule],
